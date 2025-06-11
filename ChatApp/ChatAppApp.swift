ca779c2ad83a9a13496884af3c8ed196eb16a9e9
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2025-06-12 01:25:27 +0530

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Yashwant Sheshkar on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
